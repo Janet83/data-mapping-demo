b8ecb9be096f5ed81a1d488cb398f89179fa8608
Janet <zan.smigiel@gmail.com>
2022-06-11 13:24:15 +0200

    data_mapping_demo - 2022.06.11 - część 1 zajęć

---
+++ src/main/java/edu/wsbbd/datamappingdemo/person/Person.java
@@ -0,0 +1,69 @@
+package edu.wsbbd.datamappingdemo.person;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Person {
+
+    private String username;
+    private String password;
+    private Boolean enabled;
+    private Date dateCreated;
+    private Long id;
+
+    public Person(String username, String password, Boolean enabled) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+        this.dateCreated = new Date();
+    }
+
+    public Person() {
+
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+}
